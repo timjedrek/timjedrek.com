3676cb8401a92f7858e4ff8aa1341fbc4e358162
Tim Jedrek <tim@timjedrek.com>
2022-08-16 17:13:23 -0500

    initial mailer setup by adding config in dev environment and generate mailer

---
+++ app/mailers/message_mailer.rb
@@ -0,0 +1,13 @@
+class MessageMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.message_mailer.new_message.subject
+  #
+  def new_message
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
