65a09e27e3ce8d9682d21a0444ca0cfe37ee66e2
Tim Jedrek <tim@timjedrek.com>
2022-08-15 22:10:04 -0500

    add randomizer for title text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def random_title_text
+    titles = [
+      "Grow your online presence!",
+      "Solutions to modernize your business!",
+      "Get your business on the internet",
+      "Custom web applications mobile friendly",
+      "Create a web presence and increase sale volume"
+    ]
+    titles[rand(titles.size)]
+  end
 end
