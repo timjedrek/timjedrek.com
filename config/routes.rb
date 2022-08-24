efa3305711d27fa643392c2a0516404facdf9377
Tim Jedrek <tim@timjedrek.com>
2022-08-24 15:08:29 -0500

    fix registration issue and limit to one user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get "users", to: "devise/sessions#new"
   end
   
-  devise_for :users, path: ''
+  devise_for :users, path: '', controllers: { registrations: "registrations" }
 
   get '/messages/confirmation', to: 'messages#confirmation'
   resources :messages
