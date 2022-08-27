96a2008a288bfc55cbdc98d2eb28ad52b379b337
Tim Jedrek <tim@timjedrek.com>
2022-08-27 05:24:12 -0500

    add mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "timjedrek.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "apikey",
+    password: ENV["SENDGRID_API_KEY"]
+  }
 end
